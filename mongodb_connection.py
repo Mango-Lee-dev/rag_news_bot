86a6adb10b0f17fc46d140a9c1b4276a5aa082b9
wootaik <fined000@naver.com>
2026-02-19 12:35:33 +0900

    Implement GDELT API integration in pipeline_mongo.py for news article retrieval and sentiment analysis. Update pipeline_api.py to process multiple organizations and adjust output format. Clean up mongodb_connection.py by removing commented-out code.

--- mongodb_connection.py
+++ mongodb_connection.py
@@ -6,13 +6,3 @@ client = MongoClient(host="localhost", port=27017)
 db = client['test']
 
 collection = db['NewsText']
-
-# item = {
-#   "title": "심성전자 주가 일시 상승",
-#   "text": "삼성전자 주가가 일시적으로 상승했다. 장중 최고치는...",
-#   "date": datetime.datetime.now()
-# }
-
-# insert_id = collection.insert_one(item).inserted_id
-
-print(collection.find_one())
